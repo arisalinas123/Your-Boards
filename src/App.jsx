085060f89302ce4527ec8f6fe4f7983056fbe39e
Ari Salinas <ari.salinas@yahoo.com>
2025-06-23 11:42:45 -0500

    Adding new libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-function App() {
+import { Button } from "@/components/ui/button"
 
+function App() {
   return (
-    <>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   )
 }
 
